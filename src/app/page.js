6e2a24ce54192414099d15cd3b95328276ea564f
Ariful Islam <arifulislam758143@gmail.com>
2024-10-02 22:53:01 +0600

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,23 @@
 import Counter from "@/components/Counter/Counter";
+import LabInfo from "@/components/Custom/LabInfo";
+import Services from "@/components/Custom/Services";
 import MessageFromChairman from "@/components/Message/MessageFromChairman";
 import NoticeNews from "@/components/NoticeNews/NoticeNews";
+
 import VisionMission from "@/components/VisionMission/VisionMission";
 
 export default function Home() {
   return (
     <main>
-<MessageFromChairman/>
-<VisionMission/>
-<NoticeNews/>
-<Counter/>
-     
+
+      <MessageFromChairman />
+      <VisionMission />
+      <NoticeNews />
+      <Counter />
+      <Services />
+      <LabInfo />
+
+
     </main>
   );
 }
---
+++ src/components/Custom/Services.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <>
+
+<div className="container my-5">
+  <h2 className="section-title" style={{ marginBottom: 40 }}>
+    FACULTIES/DISCIPLINES
+  </h2>
+  <hr />
+  <div className="row">
+    <div className="col-lg-3 col-md-6 mb-4">
+      <div className="card card-custom">
+        <div
+          className="card-img-wrapper card-img-top"
+          style={{
+            backgroundImage:
+              'url("https://st2.depositphotos.com/4021139/7734/i/450/depositphotos_77346754-stock-photo-get-results-concept-on-blackboard.jpg")'
+          }}
+        />
+        <div className="card-body">
+          <h5  className="card-title">Result Processing System</h5>
+          <a href="#" className="btn-attractive">
+            Visit Website
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="col-lg-3 col-md-6 mb-4">
+      <div className="card card-custom">
+        <div
+          className="card-img-wrapper card-img-top"
+          style={{
+            backgroundImage:
+              'url("https://hrone.cloud/wp-content/uploads/2021/05/leave-management-1.png")'
+          }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">Leave Management</h5>
+          <a href="#" className="btn-attractive">
+            Visit Website
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="col-lg-3 col-md-6 mb-4">
+      <div className="card card-custom">
+        <div
+          className="card-img-wrapper card-img-top"
+          style={{
+            backgroundImage:
+              'url("https://www.visaplace.com/wp-content/uploads/2011/01/FillingoutForm.jpg")'
+          }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">Form Fillup</h5>
+          <a href="#" className="btn-attractive">
+            Visit Website
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="col-lg-3 col-md-6 mb-4">
+      <div className="card card-custom">
+        <div
+          className="card-img-wrapper card-img-top"
+          style={{
+            backgroundImage:
+              'url("https://vakilsearch.com/blog/wp-content/uploads/2023/04/notice-format-1.webp")'
+          }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">Notice</h5>
+          <a href="#" className="btn-attractive">
+            Visit Website
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+        </>
+    )
+}
+
+export default Services
